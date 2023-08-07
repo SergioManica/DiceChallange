58dbcc8ca7ad01f0afa98a0d9c20e23794ac345f
sergio_manica <serginhomanica@hotmail.com>
2023-08-07 16:25:47 -0300

    refactoring

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ function randomDiceNumber(randomNumber1, randomNumber2) {
   for (let i = 1; i <= 6; i++) {
     randomImg1.push(`./images/dice${i}.png`);
   }
-  document
-    .querySelector(".img1")
-    .setAttribute("src", randomImg1[randomNumber1]);
+  document.querySelector(".img1").setAttribute("src", randomImg1[randomNumber1]);
 
   const randomImg2 = [];
   randomNumber2 = Math.floor(Math.random() * 6);
@@ -20,9 +18,7 @@ function randomDiceNumber(randomNumber1, randomNumber2) {
   for (let i = 1; i <= 6; i++) {
     randomImg2.push(`./images/dice${i}.png`);
   }
-  document
-    .querySelector(".img2")
-    .setAttribute("src", randomImg2[randomNumber2]);
+  document.querySelector(".img2").setAttribute("src", randomImg2[randomNumber2]);
 
   if (randomNumber1 > randomNumber2) {
       document.querySelector("h1").innerHTML = "🚩 Player 1 win!";
